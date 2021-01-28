7e8a87fdf424b009b02ac21e98165b9892aa7cd1
BatistaTony <batistatomeoliveira96@gmail.com>
2021-01-28 15:57:47 +0100

    test: write a test for hook usePrevious

---
+++ modules/hooks/__tests__/use-previous.test.ts
@@ -0,0 +1,30 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { usePrevious } from './../index';
+
+describe(' Testing a usePrevious Hook ', () => {
+  it('Returning a undefined as previous because does not exist previous one', () => {
+    const values = [1, 2, 3, 4];
+
+    const { result } = renderHook(() => usePrevious(values));
+
+    act(() => {
+      result.current;
+    });
+
+    expect(result.current).toBeUndefined();
+  });
+
+  it('returning a previous value when rerender a page', () => {
+    const values = [1, 2, 3, 4];
+
+    const { result, rerender } = renderHook(() => usePrevious(values));
+
+    const nextValues = 5;
+
+    values.push(nextValues);
+
+    rerender(true);
+
+    expect(result.current).toBe(values);
+  });
+});

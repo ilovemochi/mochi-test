f817058c6e38245ce616a517b7994c0e0e4b0dfe
RTAndrew <anderson_rodax@hotmail.com>
2021-02-21 21:03:06 +0100

    test(hook): finalize the useClickOutsideListener

--- modules/hooks/use-click-outside-listener-ref.test.ts
+++ modules/hooks/use-click-outside-listener-ref.test.ts
@@ -5,27 +5,45 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useClickOutsideListenerRef from './use-click-outside-listener-ref';
 
 describe('useClickOutsideListenerRef hook', () => {
-  it('it should create a script element for the google script', () => {
-    let div = document.createElement('div');
-    div.id = 'div';
-    div.textContent = 'rodax';
-    document.body.appendChild(div);
+  const div = document.createElement('div');
+  div.id = 'div';
+  div.textContent = 'rodax';
 
+  function createHtmlElement(div: HTMLElement) {
+    document.body.appendChild(div);
+  }
+
+  function removeHtmlElement(div: HTMLElement) {
+    document.body.removeChild(div);
+  }
+
+  beforeEach(() => {
+    createHtmlElement(div);
+  });
+
+  afterEach(() => {
+    removeHtmlElement(div);
+  });
+
+  it('should call mockClick on detecting the Escape key', () => {
     const mockClick = jest.fn();
 
-    const { result, unmount, rerender } = renderHook(() => useClickOutsideListenerRef(mockClick));
-    // expect(document.querySelectorAll('script').length).toBe(1);
+    const { result } = renderHook(() => useClickOutsideListenerRef(mockClick));
 
-    // Simulate a click on a element
-    // Check if the click was from Mouse or from pressing the Escape Keyboard
+    fireEvent.keyUp(screen.getByText('rodax'), { key: 'Escape', code: 'Escape' });
+    expect(mockClick).toHaveBeenCalled();
+  });
 
-    // expect(mockClick).toHaveBeenCalled();
-    // expect(document.querySelectorAll('#div')).
+  // This will never get called
+  // because the custom hook does not have
+  // a ref element
+  // instead, the ref is set to null.
+  it('should NOT CALL mockClick, on detecting mouseClick', () => {
+    const mockClick = jest.fn();
 
-    console.debug(document.body);
+    const { result  } = renderHook(() => useClickOutsideListenerRef(mockClick));
 
-    fireEvent.click(screen.getByText('rodax'));
-
+    fireEvent.click(document.body);
     expect(mockClick).not.toHaveBeenCalled();
   });
 });
